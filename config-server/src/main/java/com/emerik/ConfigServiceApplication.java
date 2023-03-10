386e61298471e3a101da6daac283a55c7f84c459
erick trujillo <erickstm22@gmail.com>
2023-03-09 23:28:26 -0500

    config-server

--- config-server/src/main/java/com/emerik/ConfigServiceApplication.java
+++ config-server/src/main/java/com/emerik/ConfigServiceApplication.java
@@ -1,11 +1,11 @@
+package com.emerik;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
-@ComponentScan("src.main.java")
 public class ConfigServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class,args);
